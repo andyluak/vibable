17ec4f084918a7414d622cb158088cb4352a3baa
Alex Tirim <49715292+andyluak@users.noreply.github.com>
2025-07-13 15:52:56 +0300

    feat(page, inngest) - add input field for user input, update TRPC mutation to accept value, and integrate Inngest agent for code generation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import { toast } from "sonner";
 
 export default function Page() {
+  const [value, setValue] = useState("");
   const trpc = useTRPC();
   const invoke = useMutation(
     trpc.invoke.mutationOptions({
@@ -16,11 +19,17 @@ export default function Page() {
   );
 
   return (
-    <div>
+    <div className='h-screen flex flex-col items-center justify-center gap-4'>
+      <Input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className='w-full max-w-md'
+      />
+
       <Button
         disabled={invoke.isPending}
         onClick={() => {
-          invoke.mutate({ email: "test@test.com" });
+          invoke.mutate({ value });
         }}
       >
         Invoke background job
--- src/inngest/function.ts
+++ src/inngest/function.ts
@@ -1,10 +1,27 @@
 import { inngest } from "./client";
+import { createAgent, openai } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
-    return { message: `Hello ${event.data.email}!` };
+    await step.sleep("wait-a-moment", "5s");
+
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system:
+        "You are an expert nextjs developer. You write readable, maintainable, and performant code. You write simple nextjs snippets.",
+      model: openai({
+        model: "gpt-4o",
+        apiKey: process.env.OPEN_ROUTER_API_KEY,
+        baseUrl: "https://openrouter.ai/api/v1",
+      }),
+    });
+
+    const { output } = await codeAgent.run(
+      `Write the following code snippet: ${event.data.value}`,
+    );
+
+    return { output };
   },
 );
